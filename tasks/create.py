0256402b39add3f6d6943b1d454c221261f3b793
Alejandro Lopez <alejandrolopez1592@gmail.com>
2024-01-24 15:08:40 -0700

    feat: Build method to create tasks, and add call to serverless.yml

---
+++ tasks/create.py
@@ -0,0 +1,35 @@
+import json
+import logging
+import os
+import time
+import uuid
+
+import boto3
+dynamodb = boto3.resource('dynamodb')
+
+def create(event, context):
+    data = json.loads(event['body'])
+    if 'text' not in data:
+        logging.error("Validation Failed")
+        raise Exception("Couldn't create the task item.")
+    
+    timestamp = str(time.time())
+    
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+    
+    item = {
+        'id': os.environ['PHONE_NUMBER'],
+        'text': data['text'],
+        'complete': False,
+        'createdAt': timestamp,
+        'updatedAt': timestamp,
+        }
+    
+    table.put_item(Item=item)
+    
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(item)
+        }
+    
+    return response
